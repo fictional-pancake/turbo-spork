7dcb1308d95f6841ba4ceaedf14ef0bfcef455be
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-02-29 13:56:11 +0000

    We have a program now

---
+++ src/fictionalpancake/turbospork/TurboSpork.java
@@ -0,0 +1,14 @@
+package fictionalpancake.turbospork;
+
+import javax.swing.*;
+
+public class TurboSpork {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JLabel unfinishedLabel = new JLabel("This isn't a game yet");
+        frame.add(unfinishedLabel);
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
